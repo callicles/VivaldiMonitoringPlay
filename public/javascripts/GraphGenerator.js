0ab19795451ad8ca18d07dfb7eae343670d1233f
Nicolas Joseph <nikel092@gmail.com>
2014-01-07 14:03:59 +0100

    error handling ui + start on js

--- public/javascripts/GraphGenerator.js
+++ public/javascripts/GraphGenerator.js
@@ -6,6 +6,25 @@
  * To change this template use File | Settings | File Templates.
  */
 
-function getNodeList(networkId){
-    $.ajax()
+function getCoordinateNumberPerNode() {
+    return $("input[name=coordinatePerNode]:checked","#coordinateParamForm").val();
+}
+
+function showError(errorToShow){
+    $("#notifications").append('<div data-alert class="alert-box warning">'+errorToShow+'<a href="#" class="close">&times;</a> </div>');
+    $("#notifications").foundation();
+}
+
+function handleCooordinate (data){
+
+}
+
+function getCoordinateList(networkId){
+    $.ajax({
+        url: "http://"+window.location.hostname+"/networks/"+networkId+"/coordinates",
+        data: {"limit": getCoordinateNumberPerNode()},
+        accepts: "application/json",
+        error: showError(e),
+        success: handleCooordinate(data)
+    });
 }
