d46037e970ab66439cdc7754094e6418b8fce8f1
Nicolas Joseph <nikel092@gmail.com>
2014-01-07 22:12:04 +0100

    working coordinate map + clean automatic tests

--- public/javascripts/commons.js
+++ public/javascripts/commons.js
@@ -11,6 +11,10 @@ function getCoordinateNumberPerNode() {
     return $("input[name=coordinatePerNode]:checked","#coordinateParamForm").val();
 }
 
+function getNetworkSelectedId(){
+    return $("select[name=network]").find(":selected").val();
+}
+
 function showError(status,errorToShow){
     $("#notifications").append('<div data-alert class="alert-box warning">'+status+" :"+errorToShow+'<a href="#" class="close">&times;</a> </div>');
 }
@@ -42,6 +46,11 @@ function handleCoordinate(data) {
     var matrix = [["x"]];
     var newRow = [];
 
+    var minX = data[0][0].x;
+    var minY = data[0][0].y;
+    var maxX = data[0][0].x;
+    var maxY = data[0][0].y;
+
     for (var i = 0 ; i<data.length ; i ++ ){
         matrix[0].push("node : "+data[i][0].nodeId);
     }
@@ -52,6 +61,11 @@ function handleCoordinate(data) {
             newRow[0] = data[i][j].x;
             newRow[i+1] = data[i][j].y;
             matrix.push(newRow);
+
+            minX = (data[i][j].x < minX) ? data[i][j].x : minX ;
+            minY = (data[i][j].y < minY) ? data[i][j].y : minY ;
+            maxX = (data[i][j].x > maxX) ? data[i][j].x : maxX ;
+            maxY = (data[i][j].y > maxY) ? data[i][j].y : maxY ;
         }
     }
 
@@ -60,8 +74,8 @@ function handleCoordinate(data) {
 
     var options = {
         title: 'Nodes Coordinates',
-        hAxis: {title: 'x', minValue: 0, maxValue: 15},
-        vAxis: {title: 'y', minValue: 0, maxValue: 15}
+        hAxis: {title: 'x', minValue: minX-2, maxValue: maxX+2},
+        vAxis: {title: 'y', minValue: minY-2, maxValue: maxY+2}
     };
 
     var chart = new google.visualization.ScatterChart(document.getElementById('chart_div'));
