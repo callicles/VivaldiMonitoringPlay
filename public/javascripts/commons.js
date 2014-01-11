9fe691c7c7c6ca55883f0e9d6c7c8baad3bfe1c8
Nicolas Joseph <nikel092@gmail.com>
2014-01-11 14:34:31 +0100

    add network interface

--- public/javascripts/commons.js
+++ public/javascripts/commons.js
@@ -13,8 +13,9 @@ function getSelectedId(object){
     return $("select[name="+object+"]").find(":selected").val();
 }
 
-function showError(status,errorToShow){
-    $("#notifications").append('<div data-alert class="alert-box warning">'+status+" :"+errorToShow+'<a href="#" class="close">&times;</a> </div>');
+function showError(content, status,errorToShow){
+    $("#notifications").append('<div data-alert class="alert-box warning">'+status+" :"+errorToShow+' - '+content+'<a href="#" class="close">&times;</a> </div>');
+    $(document).foundation();
 }
 
 function nullArray(length){
@@ -25,22 +26,73 @@ function nullArray(length){
     return nullRow;
 }
 
+function createNetwork(){
+    var networkName =  $("#newNetworkName").val()
+    $.ajax({
+        type: "POST",
+        url: "http://"+window.location.host+"/networks/",
+        contentType: "application/json",
+        dataType: "json",
+        data: JSON.stringify({"networkName": networkName}),
+        error: function( jqXHR, textStatus, errorThrown ){
+            showError(jqXHR.responseText , textStatus, errorThrown);
+        },
+        success:  function(data){
+            showNetworkData(data);
+        }
+    });
+}
+
+function showNetworkData(data){
+    var modal = $("#addNetworkModalContent");
+    modal.before('<div data-alert class="alert-box success radius">Network created : {id:'+data.id+', networkName: '+data.networkName+'}<a href="#" class="close">&times;</a></div>');
+    $(document).foundation();
+    updateNetworkList();
+}
+
+function updateNetworkList(){
+    var networkList = $("select[name=network]");
+    var selectedNetwork = getSelectedId("network");
+
+    $.ajax({
+        url: "http://"+window.location.host+"/networks/",
+        accepts: "application/json",
+        error: function( jqXHR, textStatus, errorThrown ){
+            showError(textStatus, errorThrown);
+        },
+        success:  function( data ){
+            networkList.empty()
+            for (var i = 0 ; i < data.length ; i++){
+                networkList.append('<option value="'+data[i].id+'">'+data[i].networkName+'</option>');
+            }
+            networkList.find("[value="+selectedNetwork+"]").first().prop("selected",true);
+        }
+    });
+}
+
 var automaticUpdate;
 
 function toggleRefresh(toExecute){
     var $icon = $( "#fi-refresh" ),
         animateClass = "icon-refresh-animate",
-        $button = $('#refreshButton');
+        $button = $('#refreshButton'),
+        $addNetworkButton = $('#addNetworkButton');
 
     if($icon.hasClass(animateClass)){
         $icon.removeClass( animateClass );
         $button.removeClass( "success" );
+        $addNetworkButton.removeAttr("style");
+        $addNetworkButton.prop('disabled', false);
         clearInterval(automaticUpdate);
+
     }else {
         $icon.addClass( animateClass );
         $button.addClass( "success" );
+        $addNetworkButton.prop('disabled', true);
+        $addNetworkButton.css("background","#b7b7b7");
         automaticUpdate = setInterval(toExecute,1000);
     }
+    $(document).foundation();
 }
 
 
